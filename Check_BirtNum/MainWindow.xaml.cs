dbdcdeefd1e0adbf0167c436a0f8d1c041b912b0
David Blanár <blanada19@sps-prosek.cz>
2022-02-14 19:45:38 +0100

    Add project files.

---
+++ Check_BirtNum/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Check_BirtNum
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            ResultWin win = new ResultWin();
+            string input = Input.Text.ToString();
+
+            win.NumberShow.Content = input;
+
+            win.Show();
+            this.Close();
+
+        }
+    }
+}
---
+++ Check_BirtNum/ResultWin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Check_BirtNum
+{
+    /// <summary>
+    /// Interaction logic for ResultWin.xaml
+    /// </summary>
+    public partial class ResultWin : Window
+    {
+        public ResultWin()
+        {
+            InitializeComponent();
+        }
+
+        private void Return_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow winMain = new MainWindow();
+
+            winMain.Input.Text = "";
+
+            winMain.Show();
+            this.Close();
+        }
+    }
+}
