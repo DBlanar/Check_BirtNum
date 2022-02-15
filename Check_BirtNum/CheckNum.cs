55b987f3921812bae376bf6476349be90ee83f10
David Blanár <blanada19@sps-prosek.cz>
2022-02-15 17:56:08 +0100

    v0.4; hotovy interface, chybi kontrola/logika

---
+++ Check_BirtNum/CheckNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_BirtNum
+{
+    class CheckNum
+    {
+        bool Check0(string a)
+        {
+            bool b = false;
+            int c;
+
+            try
+            {
+                c = Convert.ToInt32(a);
+            }
+            catch (Exception)
+            {
+
+                
+            }
+
+            return b;
+        }
+    }
+}
